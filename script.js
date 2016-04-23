4f38eff7b1d8e9adc47ef5728e59012594e45a51
Timothy Nguyen <shimoshixp@gmail.com>
2016-04-22 18:05:43 -0700

    starting angular version, got the number to compare on click

--- script.js
+++ script.js
@@ -1,58 +1,80 @@
-/**
- * Created by Timmy on 2/23/2016.
- */
-var the_number = null;
+var app = angular.module('myApp', []);
 
-function pick_number() {
-    var random_number = Math.floor((Math.random() * 10) + 1);
-    return random_number;
-}
-function make_guess() {
-    var the_guess = $("#guess_input").val();
-    if (the_guess >= 1 && the_guess <= 10) {
-        if (Math.abs(the_guess - the_number) >= 2) {
-            if ($("body").attr('class') == 'cold') {
-                if (the_guess - the_number > 1) {
-                    $("#response_div").text('You\'re still too high!');
-                }
-                else {
-                    $("#response_div").text('You\'re still too low!');
-                }
-            }
-            else {
-                if (the_guess - the_number > 1) {
-                    $("#response_div").text('You\'re too high!');
-                    $("body").removeClass().addClass("cold");
-                }
-                else {
-                    $("#response_div").text('You\'re too low!');
-                    $("body").removeClass().addClass("cold");
-                }
-            }
-        }
-        else if (Math.abs(the_guess - the_number) == 1) {
-            if ($("body").attr('class') != "warm") {
-                $("#response_div").text('You\'re close!');
-                $("body").removeClass().addClass("warm");
-            }
-            else {
-                $("#response_div").text('You\'re still close!');
-            }
+app.service('myService', function() {
+    var self = this;
+    this.realNum = Math.floor((Math.random() * 10) + 1);
+    this.compareNum = function(guessNum) {
+        if (guessNum == self.realNum) {
+            console.log("same");
         }
         else {
-            $("#response_div").text('WOOHOO! You guessed it!');
-            $("body").removeClass().addClass('hot');
+            console.log("different");
         }
     }
-    else {
-        $("#response_div").text('Try a CORRECT input...');
-    }
-}
-
-$(document).ready(function () {
-    the_number = pick_number();
-    console.log('Cheater! The answer is ' + the_number);
-    $("#submit_button").click(function () {
-        make_guess();
-    });
 });
+
+app.controller('myController', function(myService) {
+    this.compareNum = myService.compareNum;
+});
+
+
+
+
+
+
+
+//var the_number = null;
+//
+//function pick_number() {
+//    var random_number = Math.floor((Math.random() * 10) + 1);
+//    return random_number;
+//}
+//function make_guess() {
+//    var the_guess = $("#guess_input").val();
+//    if (the_guess >= 1 && the_guess <= 10) {
+//        if (Math.abs(the_guess - the_number) >= 2) {
+//            if ($("body").attr('class') == 'cold') {
+//                if (the_guess - the_number > 1) {
+//                    $("#response_div").text('You\'re still too high!');
+//                }
+//                else {
+//                    $("#response_div").text('You\'re still too low!');
+//                }
+//            }
+//            else {
+//                if (the_guess - the_number > 1) {
+//                    $("#response_div").text('You\'re too high!');
+//                    $("body").removeClass().addClass("cold");
+//                }
+//                else {
+//                    $("#response_div").text('You\'re too low!');
+//                    $("body").removeClass().addClass("cold");
+//                }
+//            }
+//        }
+//        else if (Math.abs(the_guess - the_number) == 1) {
+//            if ($("body").attr('class') != "warm") {
+//                $("#response_div").text('You\'re close!');
+//                $("body").removeClass().addClass("warm");
+//            }
+//            else {
+//                $("#response_div").text('You\'re still close!');
+//            }
+//        }
+//        else {
+//            $("#response_div").text('WOOHOO! You guessed it!');
+//            $("body").removeClass().addClass('hot');
+//        }
+//    }
+//    else {
+//        $("#response_div").text('Try a CORRECT input...');
+//    }
+//}
+//
+//$(document).ready(function () {
+//    the_number = pick_number();
+//    console.log('Cheater! The answer is ' + the_number);
+//    $("#submit_button").click(function () {
+//        make_guess();
+//    });
+//});
